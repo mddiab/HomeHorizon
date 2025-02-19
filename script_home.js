5dd78205645d2c46d82f7d0546d640ae82d37b30
Diab <moe.diab2003@gmail.com>
2025-02-19 09:58:09 +0200

    Updated Dark Mode

--- script_home.js
+++ script_home.js
@@ -1,14 +1,39 @@
 let toggleButton = document.getElementById('toggleModeBtn');
 let body = document.body;
+let locationcards = document.querySelectorAll(".location-card")
+let sections = document.querySelectorAll(".section")
 
-    toggleButton.addEventListener('click', () => {
-        if (body.classList.contains('light-mode')) {
-            body.classList.remove('light-mode');
-            body.classList.add('dark-mode');
-            toggleButton.textContent = '🌕';
-        } else {
-            body.classList.remove('dark-mode');
-            body.classList.add('light-mode');
-            toggleButton.textContent = '🌙';
-        }
-    });
+toggleButton.addEventListener('click', () => {
+    if (toggleButton.textContent === '🌙') {
+        DarkMode();
+    } else {
+        LightMode();
+    }
+});
+
+function DarkMode() {
+    toggleButton.textContent = '🌕';
+    body.style.color = "white";
+    
+    for (let i = 0; i < locationcards.length; i++) {
+        locationcards[i].style.background = 'linear-gradient(to bottom, #333333, #666666)';
+    }
+    
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].style.backgroundColor = 'black';
+    }
+}
+
+
+function LightMode() {
+    toggleButton.textContent = '🌙';
+    body.style.color = "black";
+    
+    for (let i = 0; i < locationcards.length; i++) {
+        locationcards[i].style.background = 'linear-gradient(to bottom, #f0f0f0, #d3d3d3)';
+    }
+
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].style.backgroundColor = '#fAf9f6';
+    }
+}
