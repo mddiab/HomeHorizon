3ac5aae17d0c787ca71f74298b80416048e1ce85
Diab <moe.diab2003@gmail.com>
2025-03-04 05:54:48 +0200

    Added Variables for Dark Mode on Home Page (Testing it before pushing to the rest of the pages)

--- Home/script_home.js
+++ Home/script_home.js
@@ -1,41 +1,12 @@
 let toggleButton = document.getElementById('toggleModeBtn');
 let body = document.body;
-let locationcards = document.querySelectorAll(".location-card")
-let sections = document.querySelectorAll(".section")
 
 toggleButton.addEventListener('click', () => {
-    if (toggleButton.textContent === '🌙') {
-        DarkMode();
+    if (body.classList.contains("dark-mode")) {
+        body.classList.remove("dark-mode");
+        toggleButton.textContent = '🌙';
     } else {
-        LightMode();
+        body.classList.add("dark-mode");
+        toggleButton.textContent = '🌕';
     }
 });
-
-function DarkMode() {
-    toggleButton.textContent = '🌕';
-    body.style.color = "white";
-    body.style.backgroundColor = "black";
-    
-    for (let i = 0; i < locationcards.length; i++) {
-        locationcards[i].style.background = 'linear-gradient(to bottom, #333333, #666666)';
-    }
-    
-    for (let i = 0; i < sections.length; i++) {
-        sections[i].style.backgroundColor = 'black';
-    }
-}
-
-
-function LightMode() {
-    toggleButton.textContent = '🌙';
-    body.style.color = "black";
-    body.style.backgroundColor = "white";
-    
-    for (let i = 0; i < locationcards.length; i++) {
-        locationcards[i].style.background = 'linear-gradient(to bottom, #f0f0f0, #d3d3d3)';
-    }
-
-    for (let i = 0; i < sections.length; i++) {
-        sections[i].style.backgroundColor = '#fAf9f6';
-    }
-}
