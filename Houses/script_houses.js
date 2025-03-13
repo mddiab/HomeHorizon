5c4c2d9a6b31d4e769ef8b34280ffb155730e94d
Diab <moe.diab2003@gmail.com>
2025-03-13 11:08:53 +0200

    Added Houses Folder + Files

---
+++ Houses/script_houses.js
@@ -0,0 +1,12 @@
+let toggleButton = document.getElementById('toggleModeBtn');
+let body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    if (body.classList.contains("dark-mode")) {
+        body.classList.remove("dark-mode");
+        toggleButton.textContent = '🌙';
+    } else {
+        body.classList.add("dark-mode");
+        toggleButton.textContent = '🌕';
+    }
+});
